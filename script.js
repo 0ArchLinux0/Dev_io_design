338103558e08094443519e90c6a280883bc31ac8
0ArchLinux0 <blueblueriz1646@daum.net>
2020-11-17 08:02:25 +0900

    .

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ if (navigator.platform) {
 const inner = document.querySelector("#lead");
 const section = document.querySelector("#lead-down");
 const sitevisible=document.querySelector("*");
-var warning = document.getElementById("warning");
+const siteloading=document.querySelector("#siteLoading");
+let warning = document.getElementById("warning");
+
+window.onload= (function(){ 
+    console.log("wainting...");
+    setTimeout(function(){
+   siteloading.style.visibility = 'hidden';
+   }, 1000);
+
+    
+    console.log("hidden"); 
+   
+    }); //ms
 
 console.log("offset Top:" + section.offsetTop);
 console.log("window:" + window.pageYOffset);
@@ -29,6 +41,7 @@ if (isMobile) {
             warning.style.visibility = 'hidden';
         } else if (window.matchMedia("(orientation: portrait)").matches) {
         	alert("Use Portrait mode!!");
+            window.scrollTo(0,0);
             sitevisible.style.visibility = 'hidden';
             warning.style.visibility = 'visible';
         }
@@ -37,55 +50,3 @@ if (isMobile) {
 }
 
 
-/*
-if (isMobile) {*/
-/*if (window.matchMedia("(orientation: landscape)").matches) {
-    alert("In LandScape mode might not work well");
-    warn.setAttribute("visibility", "visible");
-    warn.setAttribute("position", "fixed");
-    warn.setAttribute("font-size", "100px");
-    warn.setAttribute("font-weight", "800");
-    warn.setAttribute("z-index", "2000");
-    warn.setAttribute("color", "red");
-}
-
-if (window.matchMedia("(orientation: portrait)").matches) {
-    alert("Portrait");
-    warn.setAttribute("visibility", "hidden");
-    warn.setAttribute("position", "fixed");
-    warn.setAttribute("font-size", "100px");
-    warn.setAttribute("font-weight", "800");
-    warn.setAttribute("z-index", "2000");
-    warn.setAttribute("color", "red");
-}*/
-/*}
- */
-
-
-
-
-/*$(function(){
-	
-	var $window = $(window);		//Window object
-	
-	var scrollTime = 1.2;			//Scroll time
-	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
-		
-	$window.on("mousewheel DOMMouseScroll", function(event){
-		
-		event.preventDefault();	
-										
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
-			
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
-				autoKill: true,
-				overwrite: 5							
-			});
-					
-	});
-	
-});*/
